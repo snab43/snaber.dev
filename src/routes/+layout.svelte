0ed965c0534b1c7720216ea1b0e88287c81dbe28
S. Naber <94073283+snab43@users.noreply.github.com>
2023-03-17 11:44:23 -0400

    Update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	</div>
 	<div id="ufo">
 		<a href="/secret">
-			<img id="ufo" src="https://web.archive.org/web/20011111174126/http://www.geocities.com:80/clipart/pbi/pictures/Sanrio/badtz_pandaba_blinkship.gif" alt="UFO guy">
+			<img id="ufo" src="/assets/gif/ufo-guy.gif" alt="UFO guy">
 		</a>
 	</div>
 </header>
@@ -20,30 +20,117 @@
 -->
 
 <div id="content-container">
-	<div id="content">
+	<div id="content" class="section">
 		<slot></slot>
 	</div>
 	<div id="panel">
-		<nav>
+		<nav class="section">
 			<h2>This Galaxy</h2>
 			<ul>
-				<li><img src="/assets/planet-0.png" alt=""> <a href="/">home</a></li>
-				<li><img src="/assets/planet-1.png" alt=""> <a href="/portfolio">portfolio</a></li>
-				<li><img src="/assets/planet-2.png" alt=""> <a href="/sasquatch">sasquatch</a></li>
+				<li><a href="/"><img src="/assets/planet-0.png" alt="">home</a></li>
+				<li><a href="/portfolio"><img src="/assets/planet-1.png" alt="">portfolio</a></li>
+				<li><a href="/sasquatch"><img src="/assets/planet-2.png" alt="">sasquatch</a></li>
 			</ul>
-			<h2 style="margin-top: 12px;">Other Galaxies</h2>
+		</nav>
+		<nav class="section">
+			<h2>Other Galaxies</h2>
 			<ul>
-				<li><img src="/assets/planet-unknown.png" alt=""> <a href="https://www.linkedin.com/in/snaber" target="_blank">linkedin</a></li>
-				<li><img src="/assets/planet-unknown.png" alt=""> <a href="https://www.github.com/snab43" target="_blank">github</a></li>
+				<li><a href="https://www.linkedin.com/in/snaber" target="_blank"><img src="/assets/planet-unknown.png" alt="">linkedin</a></li>
+				<li><a href="https://www.github.com/snab43" target="_blank"><img src="/assets/planet-unknown.png" alt="">github</a></li>
 			</ul>
 		</nav>
 	</div>
 </div>
 
-<footer id="footer">
+<footer id="footer" class="section">
 	<span id="copyright">Copyright &copy; 2023 snaber.dev. All Rights Reserved.</span>
 </footer>
 
 <div style="text-align: center;">
-	<img src="/assets/astronaut.gif" alt="Astronaut">
+	<img src="/assets/gif/astronaut.gif" alt="Astronaut">
 </div>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 12px;
+	}
+
+	footer {
+		font-size: 0.8rem;
+		text-align: center;
+		color: #c0c0c0;
+	}
+
+	#content-container {
+		display: flex;
+	}
+
+	#content {
+		margin-right: 12px;
+		width: 100%;
+	}
+
+	#panel {
+		width: fit-content;
+	}
+
+	#panel ul {
+		list-style-type: none;
+		padding: 0;
+		margin: 0;
+	}
+
+	#panel a {
+		display: flex;
+		align-items: center;
+	}
+
+	#panel img {
+		margin-right: 4px;
+	}
+
+	.section {
+		margin-bottom: 12px;
+		padding: 12px;
+		background: repeat-x url("../assets/texture-bg.png") #0b101a;
+		/* border: 2px solid #1a243c; */
+		border: 2px solid #3b4866;
+		border-radius: 6px;
+		transition: box-shadow 0.3s ease;
+	}
+
+	.section:hover {
+		box-shadow: 0 0 30px 5px rgba(208, 211, 219, 0.089), 0 0 3px 2px #234fb654;
+		transition: box-shadow 0.3s ease;
+	}
+
+	h2 {
+		white-space: nowrap;
+	}
+
+	@media only screen and (max-width: 500px) {
+		#content-container {
+			flex-direction: column-reverse;
+		}
+
+		#content {
+			max-width: fit-content;
+			margin-right: 0 !important;
+		}
+
+		#panel {
+			width: 100%;
+		}
+
+		#panel ul {
+			display: flex;
+		}
+
+		#panel ul li:not(:last-child) {
+			margin-right: 16px;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,53 @@
 <h1>+ HOME PLANET +</h1>
 
-<h2>Welcome!</h2>
-<p>my name is <span style="font-family: 'VT323', monospace; font-size: 1.2rem; letter-spacing: 1px; color: #ff7777;"><strong>snaber</strong></span>. i'm a web developer and this is my cool website! i also have a cat named <a href="/sasquatch">sasquatch</a>.</p>
-<div style="text-align: center;">
-	<img src="/assets/gif/balloons.gif" alt="Bouncing Balloons">
-</div>
+<h2>Welcome to snaber's galaxy!</h2>
+<p>i'm a <strong>web developer</strong>, an <strong>artist</strong>, and this is my <strong class="rainbow"><span>g</span><span>a</span><span>l</span><span>a</span><span>x</span><span>y</span><span>!</span></strong></p>
+<p>it's inspired by the internet i grew up on. it's an ode to all those sites i wrote in notepad.exe as a kid, but this time made with modern tools like <strong>SvelteKit</strong> (you can see the source code <a href="https://github.com/snab43/snaber.dev" target="_blank">here</a>).</p>
+<p>i also have a cat named <strong>sasquatch</strong> who you can find at <a href="/sasquatch">planet sasquatch</a>.</p>
 
-<hr>
+<div class="star-row">
+</div>
 
 <h2>Thanks!</h2>
 <p>thanks for visiting my cool website. i hope to see you again soon!</p>
 <img src="/assets/gif/bye.gif" alt="Waving Bye Animation">
+
+<style>
+	strong {
+		color: #ff8686;
+	}
+
+	.star-row {
+		height: 20px;
+		margin: 20px 0;
+		background: url('/assets/gif/star-row.gif') repeat-x;
+	}
+
+	.rainbow span:nth-child(10n + 1) {
+		color: #ef5350;
+	}
+
+	.rainbow span:nth-child(10n + 2) {
+		color: #e98b1f;
+	}
+
+	.rainbow span:nth-child(10n + 3) {
+		color: #0ef506;
+	}
+
+	.rainbow span:nth-child(10n + 4) {
+		color: #19cc64;
+	}
+
+	.rainbow span:nth-child(10n + 5) {
+		color: #1571e9;
+	}
+
+	.rainbow span:nth-child(10n + 6) {
+		color: #6d15e0;
+	}
+
+	.rainbow span:nth-child(10n + 7) {
+		color: #ca2aa2;
+	}
+</style>
