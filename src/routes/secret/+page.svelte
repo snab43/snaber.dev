717312518dd50894a0e6fe1750f5f13508b1408e
S. Naber <94073283+snab43@users.noreply.github.com>
2023-03-17 13:39:33 -0400

    Fixed counter

--- src/routes/secret/+page.svelte
+++ src/routes/secret/+page.svelte
@@ -5,4 +5,4 @@
 <h2>How Special Are You?</h2>
 <p>This many people have found this page so far:</p>
 
-<div id="sfcq8jr6gu5rhl8yhbaq4qhgg4zd8gyn26r"></div> <script type="text/javascript" src="https://counter4.optistats.ovh/private/counter.js?c=q8jr6gu5rhl8yhbaq4qhgg4zd8gyn26r&down=async" async></script><noscript><a href="https://www.freecounterstat.com" title="web page counter"><img src="https://counter4.optistats.ovh/private/freecounterstat.php?c=q8jr6gu5rhl8yhbaq4qhgg4zd8gyn26r" border="0" title="web page counter" alt="web page counter"></a></noscript> 
+<div id="sfcq8jr6gu5rhl8yhbaq4qhgg4zd8gyn26r"></div> <script type="text/javascript" src="https://counter4.optistats.ovh/private/counter.js?c=q8jr6gu5rhl8yhbaq4qhgg4zd8gyn26r&down=async" async></script> <br><a href="https://www.freecounterstat.com">web page counter</a><noscript><a href="https://www.freecounterstat.com" title="web page counter"><img src="https://counter4.optistats.ovh/private/freecounterstat.php?c=q8jr6gu5rhl8yhbaq4qhgg4zd8gyn26r" border="0" title="web page counter" alt="web page counter"></a></noscript> 
