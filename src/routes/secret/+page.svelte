8a0966a1211ea54e486eae4bc3b2d4ee05771e57
S. Naber <94073283+snab43@users.noreply.github.com>
2023-03-17 13:13:54 -0400

    Fixed build error

--- src/routes/secret/+page.svelte
+++ src/routes/secret/+page.svelte
@@ -1,5 +1,3 @@
 <h1>+ SECRET PLANET +</h1>
 <h2>You Did It!</h2>
 <p>wow! congrats you found the secret planet.</p>
-<br>
-<a href="/"><img src="/assets/gif/back.gif" alt="Back Button"> main site</a>
