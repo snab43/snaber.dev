420ce6ab20b553918317eaa05f0e0d436a8fdfe0
S. Naber <94073283+snab43@users.noreply.github.com>
2023-03-21 17:33:22 -0400

    Possible form fix

--- src/routes/guestbook/+page.svelte
+++ src/routes/guestbook/+page.svelte
@@ -1,20 +1,41 @@
-<!--
 <script>
 	import { onMount } from "svelte";
 
 	onMount(async () => {
-	fetch("https://api.netlify.com/api/v1/forms/{form_id}/submissions")
-	.then(response => response.json())
-	.then(data => {
-		console.log(data);
-		apiData.set(data);
-	}).catch(error => {
-		console.log(error);
-		return [];
-	});
+		// AJAX Submit
+		const handleSubmit = (event) => {
+			event.preventDefault();
+
+			const myForm = event.target;
+			const formData = new FormData(myForm);
+			
+			fetch("/", {
+				method: "POST",
+				headers: { "Content-Type": "application/x-www-form-urlencoded" },
+				body: new URLSearchParams(formData).toString(),
+			})
+				.then(() => console.log("Form successfully submitted"))
+				.catch((error) => alert(error));
+		};
+
+		document
+			.querySelector("form")
+			.addEventListener("submit", handleSubmit);
+
+		// Get submissions
+		/*
+		fetch("https://api.netlify.com/api/v1/forms/641a1fe6cb18cd0008964769/submissions")
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			apiData.set(data);
+		}).catch(error => {
+			console.log(error);
+			return [];
+		});
+		*/
 	});
 </script>
--->
 
 <h1>+ Guestbook Planet +</h1>
 <h2>Sign the Galaxybook</h2>
