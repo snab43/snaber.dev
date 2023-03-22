1d5ca4c3d9bd2aefb95e519d098a61a1229e0ea1
S. Naber <94073283+snab43@users.noreply.github.com>
2023-03-22 17:22:32 -0400

    New blog post

--- src/routes/blog/data.js
+++ src/routes/blog/data.js
@@ -1,5 +1,21 @@
 export const posts = [
 	{
+		slug: 'galaxy-radio',
+		title: 'Galaxy Radio',
+		date: '2023-03-22',
+		diameter: '70',
+		mass: '310',
+		orbitalPeriod: '-0.1223',
+		content:
+			`
+			<p>as i was star searching last night something caught my eye that i haven't seen before. it was a small rectangular box floating by the guestbook planet. i got into my spaceship to get a closer look.</p>
+			<p>upon arrival i sent out my space robot to retrieve the box and bring it into my ship. i was eager to see what awaited me in the airlook.</p>
+			<p>wow! it was dusty, but it's a old radio. i haven't seen one of these in a while. it looks like it can pick up just one station. i'm not sure where the signal is coming from, but it's somewhere in this galaxy. i love a good pirate radio broadcast.</p>
+			<p>i'll just place it down by the galaxy map and enjoy the lo-fi sounds.</p>
+			`
+	},
+
+	{
 		slug: 'blog-planet',
 		title: '"Blog Planet"',
 		date: '2023-03-21',
