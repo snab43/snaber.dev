d01caf5b37d4d34995ca2ab94a738675be42adbe
S. Naber <94073283+snab43@users.noreply.github.com>
2023-06-14 10:09:14 -0400

    Removed hire section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,6 @@
 <p>it's inspired by the internet i grew up on. it's an ode to all the websites i wrote in notepad.exe as a kid, but with modern tools like SvelteKit <i>(you can see the source code <a href="https://github.com/snab43/snaber.dev" target="_blank">on GitHub</a>)</i>. i made all the art, too.</p>
 <p>i also have a cat named sasquatch who lives on <a href="/sasquatch">planet sasquatch</a>.</p>
 
-<h2>Hire Me</h2>
-<p>i'm available for hire and seeking full time employment. my strong suit is <strong>front-end development</strong> and <strong>UI design</strong>. connect with me on <a href="https://www.linkedin.com/in/snaber">LinkedIn</a> where you'll also find my most up to date professional experience.</p>
-
-<p>i'm a strong asset to any team and have received numerous acolades from co-workers in all departments. i can provide references upon request.</p>
-
 <div class="star-row"></div>
 
 <h2>Thanks!</h2>
